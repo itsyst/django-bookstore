b6d6a43248242a733cc0ee74dd6932ee70952f52
Khaled El Hamzi <contact@elhamzi.me>
2021-07-10 02:48:11 +0200

    add class models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    ISBN = models.IntegerField(max_length=13)
+    title = models.CharField(max_length=255)
+    Description = models.CharField(max_length=255)
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
