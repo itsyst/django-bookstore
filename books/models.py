e1b9aeacf4d288fe735c336fe6fc685ec0ed60cc
Khaled El Hamzi <contact@elhamzi.me>
2021-07-10 03:39:43 +0200

    customize the genre object name

--- books/models.py
+++ books/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
