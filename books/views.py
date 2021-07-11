c504f92c63d505bab3ae39dc3ca0074157b6aa99
Khaled El Hamzi <contact@elhamzi.me>
2021-07-11 17:39:05 +0200

    render the book view

--- books/views.py
+++ books/views.py
@@ -6,3 +6,8 @@ from .models import Book
 def index(request):
     books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
+
+
+def detail(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'books/detail.html', {'book': book})
