99a00fa29dab32a54685125b1bc73334f95d8c96
Khaled El Hamzi <contact@elhamzi.me>
2024-10-18 20:26:54 +0200

    Use viewSet to combine the logic of multiple views  (CRUD)

--- books/views.py
+++ books/views.py
@@ -3,46 +3,34 @@ from django.db.models import Count
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Book, Genre
 from .serializers import BookSerializer, GenreSerializer
 
-class BookList(ListCreateAPIView): 
-    queryset = Book.objects.select_related('genre').all()
-    serializer_class = BookSerializer
-    
-    def get_serializer_context(self):
-        return  {'request': self.request} 
- 
-class BookDetail(RetrieveUpdateDestroyAPIView):
+class BookViewSet(ModelViewSet):
     queryset = Book.objects.select_related('genre').all()
     serializer_class = BookSerializer
     lookup_field = 'id'
-    
+
     def get_serializer_context(self):
-        return {'request': self.request}
- 
+        return  {'request': self.request} 
+    
     def delete(self, request, id):
         book = get_object_or_404(Book, pk=id)
         if book.number_in_stock > 0:
             return Response({'error': 'Book can not deleted.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
- 
-class GenreList(ListCreateAPIView):
-    queryset = Genre.objects.annotate(books_count=Count('books'))  # Annotate books_count here
-    serializer_class = GenreSerializer
+  
 
-    def get_serializer_context(self):
-        return {'request': self.request}
-
-class GenreDetail(RetrieveUpdateDestroyAPIView):
+class GenreViewSet(ModelViewSet):
     queryset = Genre.objects.annotate(books_count=Count('books'))  # Annotate books_count here
     serializer_class = GenreSerializer
     lookup_field = 'id'
 
     def get_serializer_context(self):
         return {'request': self.request}
-
+ 
     def delete(self, request, id):
         genre = get_object_or_404(Genre.objects.annotate(books_count=Count('books')), pk=id)
         if genre.books_count > 0:
