13fa167299538dce63efe76746925fc83d3f1496
Khaled El Hamzi <contact@elhamzi.me>
2024-10-20 05:14:59 +0200

    Updated model properties

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
+
 from uuid import uuid4
 
 
@@ -9,12 +11,12 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
     ISBN = models.CharField(max_length=13)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     number_in_stock = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(1)], default=0)
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='books')
     date_created = models.DateTimeField(default=timezone.now)
