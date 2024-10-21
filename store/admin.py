d35470106ad31748009f22e3f8893189bb0478d2
Khaled El Hamzi <contact@elhamzi.me>
2024-10-21 23:21:32 +0200

    Created user_profile and orders.

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,15 @@
-from django.utils.html import format_html, urlencode
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models.aggregates import Count
+from django.utils.html import format_html, urlencode
 from django.urls import reverse
-from store import models
+from . import models
  
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'books_count')
     list_editable = ['name']
-
+    search_fields = ['name']
+ 
     @admin.display(ordering="books_count")
     def books_count(self, genre):
         url = (reverse('admin:store_book_changelist')
@@ -23,11 +24,50 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['genre']
     exclude = ('date_created',)
     list_display = ('ISBN', 'title', 'description',
-                    'number_in_stock', 'daily_rate')
+                    'number_in_stock', 'unit_price', 'daily_rate')
     
-    list_editable = ['number_in_stock']
+    list_editable = ['unit_price']
+    list_filter = ['genre', 'last_updated']
     search_fields = ['ISBN', 'title']
+    list_select_related = ['genre']
     list_per_page = 10
  
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'orders']
+    list_per_page = 10
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['book']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id', 'placed_at', 'customer']
--- store/models.py
+++ store/models.py
@@ -1,10 +1,37 @@
+from django.contrib import admin
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
 
 from uuid import uuid4
+ 
 
+class Customer(models.Model):
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
 
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
@@ -25,11 +52,26 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-class Review(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    date = models.DateField(auto_now_add=True)
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+ 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='orderitems')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
@@ -44,3 +86,9 @@ class CartItem(models.Model):
 
     class META:
         unique_together = [['cart', 'book']]
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
