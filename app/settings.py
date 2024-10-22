132d09e816643f47c35cba86f2ea3497009469b9
Khaled El Hamzi <contact@elhamzi.me>
2024-10-22 23:52:01 +0200

    Added a custom serializer to get the current user.

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 
+from datetime import timedelta
 import os
 from pathlib import Path
 
@@ -148,14 +149,17 @@ REST_FRAMEWORK = {
     )
 }
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
-
 AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
+}                                                                                                                               
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1) 
 }
