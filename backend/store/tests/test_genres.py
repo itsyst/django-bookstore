4adf7e2651b46d2e42a3bc8354b5b97a16374908
itsyst <contact@elhamzi.me>
2024-11-20 21:57:00 +0100

    Create genre auth test.

---
+++ backend/store/tests/test_genres.py
@@ -0,0 +1,17 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+import pytest
+
+@pytest.mark.django_db
+class TestCreateGenre:
+    def test_if_user_is_anonymous_returns_401(self):
+        client = APIClient()
+        response = client.post('/genres/', {'name': 'Tragedy++'}) # Simulate POST request
+        print(response.json())  # This will show the error message if the request is invalid
+        
+        # Ensure the response status code is 401 (Unauthorized) for unauthenticated users
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+
+# RUN TEST: pytest
