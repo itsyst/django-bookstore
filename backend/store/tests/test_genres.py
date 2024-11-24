72e5ead63c772881623a3f123f2145a1571a7c45
itsyst <contact@elhamzi.me>
2024-11-24 13:07:42 +0100

    Add a new test for user authentication.

--- backend/store/tests/test_genres.py
+++ backend/store/tests/test_genres.py
@@ -13,7 +13,14 @@ class TestCreateGenre:
         # Ensure the response status code is 401 (Unauthorized) for unauthenticated users
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user= {})
+        response = client.post('/genres/', {'name': 'Tragedy++'}) # Simulate POST request
+        print(response.json())  # This will show the error message if the request is invalid
+        
+        # Ensure the response status code is 403 (forbiden) for none admin user
+        assert response.status_code == status.HTTP_403_FORBIDDEN
 
 # RUN TEST: pytest
 # RUN TEST: ptw
