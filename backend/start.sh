961fccd8438bd1a0af971a8841b7299c70a42281
itsyst <contact@elhamzi.me>
2024-12-10 21:49:40 +0100

    Update start up sh file.

--- backend/start.sh
+++ backend/start.sh
@@ -12,6 +12,11 @@ source /app/.venv/bin/activate
 # Run Celery in the background
 # celery -A app flower
 
+# Install the dependencies (in case anything was missed during Docker image build)
+pip install --no-cache-dir -r /app/requirements.txt
+
+# Run Django migrations
+python /app/manage.py migrate
+
 # Run the Django development server
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python /app/manage.py runserver 0.0.0.0:8000
